f495e287d00ccf0a3662e1bbf60a2ad8ebafd20f
DevaAnanda <devapradana94@gmail.com>
2025-02-21 10:27:24 +0700

    update privateroute

--- frontend/src/utils/PrivateRoute.jsx
+++ frontend/src/utils/PrivateRoute.jsx
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types';
 const PrivateRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
 
+  // Jika pengguna sudah terautentikasi dan mencoba mengakses halaman login, arahkan ke admin
+  if (isAuthenticated && window.location.pathname === '/login') {
+    return <Navigate to="/admin" replace />;
+  }
+
+  // Jika tidak terautentikasi, arahkan ke halaman login
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
